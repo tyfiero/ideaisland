4c589a77bddf762fec481445377856901d08a975
tyfiero <77432183+tyfiero@users.noreply.github.com>
2022-03-31 12:55:01 -0700

    test

--- pages/notes.js
+++ pages/notes.js
@@ -8,8 +8,6 @@ import { useSelector } from "react-redux";
 import { firebaseAdmin } from "../lib/firebaseAdmin";
 import nookies from "nookies";
 
-
-
 export const getServerSideProps = async (ctx) => {
   try {
     const cookies = nookies.get(ctx);
@@ -27,33 +25,33 @@ export const getServerSideProps = async (ctx) => {
     // either the `token` cookie didn't exist
     // or token verification failed
     // either way: redirect to the login page
-    ctx.res.writeHead(302, { Location: '/login' });
-    ctx.res.end();
+    // ctx.res.writeHead(302, { Location: '/login' });
+    // ctx.res.end();
     // The props returned here don't matter because we've
     // already redirected the user.
-    return { props: {error: err} };
+    return { props: { error: err } };
   }
 };
 
 const NotePage = (props) => {
   const unsavedChangesRedux = useSelector((state) => state.unsavedChanges);
   // const [ID, setID] = useState(null);
-// useEffect(() => {
-console.log(props.error)
-//   if(unsavedChangesRedux){
-//     window.onbeforeunload = function (e) {
-//       var message = "Your confirmation message goes here.",
-//       e = e || window.event;
-//       // For IE and Firefox
-//       if (e) {
-//         e.returnValue = message;
-//       }
-    
-//       // For Safari
-//       return message;
-//     };
-//   }
-// }, [])
+  // useEffect(() => {
+  console.log(props.error);
+  //   if(unsavedChangesRedux){
+  //     window.onbeforeunload = function (e) {
+  //       var message = "Your confirmation message goes here.",
+  //       e = e || window.event;
+  //       // For IE and Firefox
+  //       if (e) {
+  //         e.returnValue = message;
+  //       }
+
+  //       // For Safari
+  //       return message;
+  //     };
+  //   }
+  // }, [])
 
   return (
     <div className="flex flex-col mt-3 fade-effect-quick">
@@ -62,12 +60,11 @@ console.log(props.error)
       {/* </div> */}
       <div className="note-grid">
         <div className="note-grid-1 normal-box-soft h-full !rounded-2xl  min-w-[240px]">
-          <IdeaSideBar cookieUID={props.cookieUID}/>
+          <IdeaSideBar cookieUID={props.cookieUID} />
         </div>
 
         <div className="mt-1 note-grid-2">
-          <Editor cookieUID={props.cookieUID}/>
-   
+          <Editor cookieUID={props.cookieUID} />
         </div>
       </div>
     </div>
