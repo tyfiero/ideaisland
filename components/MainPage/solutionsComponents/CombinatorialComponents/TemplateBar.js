b801452b7378a1d2d75e1378b7629f62fa4e1abe
tyfiero <77432183+tyfiero@users.noreply.github.com>
2022-04-14 11:36:23 -0700

    test6

--- components/MainPage/solutionsComponents/CombinatorialComponents/TemplateBar.js
+++ components/MainPage/solutionsComponents/CombinatorialComponents/TemplateBar.js
@@ -1,13 +1,18 @@
+
 import {React, useState, useEffect} from 'react'
 import { FaCaretDown, FaChevronCircleDown, FaChevronCircleUp } from 'react-icons/fa'
 import { Cascader } from "antd";
 import "antd/dist/antd.css";
+import { useSelector, useDispatch } from "react-redux";
+import { sArrayAction } from "../../../../redux/actions";
 
 
 function TemplateBar() {
 const [template, setTemplate] = useState("How might we improve ______ with ____ to improve user satisfaction?")
 const [menuOpen, setMenuOpen] = useState(false)
 
+const dispatch = useDispatch();
+const sArray = useSelector((state) => state.sArray);
 
 
 
@@ -19,6 +24,7 @@ function onCascadeChange(value, label) {
 
     if(value.length >2){
         setTemplate(label[2].label)
+        dispatch(sArrayAction(value[2]));
 
         
     }else{
@@ -33,7 +39,7 @@ function onCascadeChange(value, label) {
     return label[label.length - 1];
   }
   return (
-    <div className='w-[80%] bg-white/60 ring-2 ring-slate-400 rounded-full flex items-center cursor-pointer md:hover:ring-4 transition select-none	py-2 relative' onClick={() => setMenuOpen(!menuOpen)}>
+    <div className='w-[80%] bg-white/60 ring-2 ring-slate-400 rounded-full flex items-center cursor-pointer md:hover:ring-4 transition select-none  py-2 relative' onClick={() => setMenuOpen(!menuOpen)}>
 
 <Cascader
             // style={{ borderRadius: "59px" }}
@@ -80,8 +86,33 @@ const options = [
           label: "Incremental Improvement",
           children: [
             {
-              value: "fortune5",
-              label: "How might we improve SAAS with encryption to protect privacy?",
+              value: [
+                { id: 0, type: "Intro", list: "How", text: "How would Elon Musk" },
+                { id: 1, type: "Verb", list: "Modifier", text: "change" },
+                { id: 2, type: "Noun", list: "Software", text: "social media" },
+                {
+                  id: 3,
+                  type: "Desired Outcome",
+                  list: "Reduce",
+                  text: "to fight spam?",
+                },
+              ],
+              label: "How would Elon Musk change social media to fight spam?",
+            },
+            {
+              value: [
+                { id: 0, type: "Intro", list: "How", text: "How might we" },
+                { id: 1, type: "Verb", list: "Modifier", text: "improve" },
+                { id: 2, type: "Noun", list: "Software", text: "tech" },
+                { id: 3, type: "Verb", list: "Action", text: "brainstorming" },
+                {
+                  id: 4,
+                  type: "Desired Outcome",
+                  list: "People",
+                  text: "for entrepreneurs?",
+                },
+              ],
+              label: "How might we improve tech brainstorming for entrepreneurs?",
             },
           ],
         },
