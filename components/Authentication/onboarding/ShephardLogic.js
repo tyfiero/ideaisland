32a1f6a6d969c4855ff8c35cd9205c036556684f
tyfiero <77432183+tyfiero@users.noreply.github.com>
2022-05-09 12:11:26 -0700

    Commit to recompile .emv on vercel

--- components/Authentication/onboarding/ShephardLogic.js
+++ components/Authentication/onboarding/ShephardLogic.js
@@ -1,31 +1,29 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState } from "react";
 // import { steps } from './steps';
-import {  ShepherdTourContext, ShepherdTour } from 'react-shepherd'
-import { useRouter } from 'next/router';
-function ShephardLogic({children}) {
-    const [shepherdTour, setShepherdTour] = useState(null);
+import { ShepherdTourContext, ShepherdTour } from "react-shepherd";
+import { useRouter } from "next/router";
+function ShephardLogic({ children }) {
+  const [shepherdTour, setShepherdTour] = useState(null);
 
+  const tour = useContext(ShepherdTourContext);
+  console.log(tour);
 
-    const tour = useContext(ShepherdTourContext);
-    console.log(tour)
+  const tourOptions = {
+    defaultStepOptions: {
+      cancelIcon: {
+        enabled: true,
+      },
+    },
+    useModalOverlay: true,
+  };
 
-    const tourOptions = {
-        defaultStepOptions: {
-          cancelIcon: {
-            enabled: true
-          }
-        },
-        useModalOverlay: true
-      };
-
-
-const router = useRouter();
-const steps = [
+  const router = useRouter();
+  const steps = [
     {
-      id: 'intro',
-      attachTo: { element: '.step-1', on: 'bottom' },
+      id: "intro",
+      attachTo: { element: ".step-1", on: "bottom" },
       beforeShowPromise: function () {
-        router.push("/")
+        router.push("/");
 
         return new Promise(function (resolve) {
           setTimeout(function () {
@@ -36,43 +34,45 @@ const steps = [
       },
       buttons: [
         {
-          classes: 'shepherd-button-secondary',
-          text: 'Exit',
-          type: 'cancel'
+          classes: "shepherd-button-secondary",
+          text: "Exit",
+          type: "cancel",
         },
         {
-          classes: 'shepherd-button-primary',
-          text: 'Back',
-          type: 'back'
+          classes: "shepherd-button-primary",
+          text: "Back",
+          type: "back",
         },
         {
-          classes: 'shepherd-button-primary',
-          text: 'Next',
-          type: 'next'
-        }
+          classes: "shepherd-button-primary",
+          text: "Next",
+          type: "next",
+        },
       ],
-      classes: 'custom-class-name-1 custom-class-name-2',
-      highlightClass: 'highlight',
+      classes: "custom-class-name-1 custom-class-name-2",
+      highlightClass: "highlight",
       scrollTo: false,
       cancelIcon: {
         enabled: true,
       },
-      title: 'Welcome to React-Shepherd!',
-      text: ['React-Shepherd is a JavaScript library for guiding users through your React app.'],
+      title: "Welcome to React-Shepherd!",
+      text: [
+        "React-Shepherd is a JavaScript library for guiding users through your React app.",
+      ],
       when: {
         show: () => {
-          console.log('show step');
+          console.log("show step");
         },
         hide: () => {
-          console.log('hide step');
-        }
-      }
+          console.log("hide step");
+        },
+      },
     },
     {
-      id: 'new-idea',
-      attachTo: { element: '.step-2', on: 'bottom' },
+      id: "new-idea",
+      attachTo: { element: ".step-2", on: "bottom" },
       beforeShowPromise: function () {
-        router.push("/notes")
+        router.push("/notes");
         return new Promise(function (resolve) {
           setTimeout(function () {
             window.scrollTo(0, 0);
@@ -82,112 +82,109 @@ const steps = [
       },
       buttons: [
         {
-          classes: 'shepherd-button-secondary',
-          text: 'Exit',
-          type: 'cancel'
+          classes: "shepherd-button-secondary",
+          text: "Exit",
+          type: "cancel",
         },
         {
-          classes: 'shepherd-button-primary',
-          text: 'Back',
-          type: 'back'
+          classes: "shepherd-button-primary",
+          text: "Back",
+          type: "back",
         },
         {
-          classes: 'shepherd-button-primary',
-          text: 'Next',
-          type: 'next'
-        }
+          classes: "shepherd-button-primary",
+          text: "Next",
+          type: "next",
+        },
       ],
-      classes: 'custom-class-name-1 custom-class-name-2 bg-white/70',
-      highlightClass: 'highlight',
+      classes: "custom-class-name-1 custom-class-name-2 bg-white/70",
+      highlightClass: "highlight",
       scrollTo: false,
       cancelIcon: {
         enabled: true,
       },
-      title: 'New Idea',
-      text: ['Create a new idea.'],
+      title: "New Idea",
+      text: ["Create a new idea."],
       when: {
         show: () => {
-          console.log('show step');
+          console.log("show step");
         },
         hide: () => {
-          console.log('hide step');
-        }
-      }
+          console.log("hide step");
+        },
+      },
     },
     {
-        id: 'finder',
-        attachTo: { element: '.step-2', on: 'bottom' },
-        beforeShowPromise: function () {
-            router.push("/solutions/finder")
+      id: "finder",
+      attachTo: { element: ".step-2", on: "bottom" },
+      beforeShowPromise: function () {
+        router.push("/solutions/finder");
 
-            // router.events.on('routeChangeStart', handleRouteChange)
+        // router.events.on('routeChangeStart', handleRouteChange)
 
-          return new Promise(function (resolve) {
-            setTimeout(function () {
-              window.scrollTo(0, 0);
-              resolve();
-            }, 500);
-          });
-        },
-        buttons: [
-          {
-            classes: 'shepherd-button-secondary',
-            text: 'Exit',
-            type: 'cancel'
-          },
-          {
-            classes: 'shepherd-button-primary',
-            text: 'Back',
-            type: 'back'
-          },
-          {
-            classes: 'shepherd-button-primary',
-            text: 'Next',
-            type: 'next'
-          }
-        ],
-        classes: 'custom-class-name-1 custom-class-name-2 bg-white/70',
-        highlightClass: 'highlight',
-        scrollTo: false,
-        cancelIcon: {
-          enabled: true,
-        },
-        title: 'New Idea',
-        text: ['Create a new idea.'],
-        when: {
-          show: () => {
-            console.log('show step');
-          },
-          hide: () => {
-            console.log('hide step');
-          }
-        }
+        return new Promise(function (resolve) {
+          setTimeout(function () {
+            window.scrollTo(0, 0);
+            resolve();
+          }, 500);
+        });
       },
+      buttons: [
+        {
+          classes: "shepherd-button-secondary",
+          text: "Exit",
+          type: "cancel",
+        },
+        {
+          classes: "shepherd-button-primary",
+          text: "Back",
+          type: "back",
+        },
+        {
+          classes: "shepherd-button-primary",
+          text: "Next",
+          type: "next",
+        },
+      ],
+      classes: "custom-class-name-1 custom-class-name-2 bg-white/70",
+      highlightClass: "highlight",
+      scrollTo: false,
+      cancelIcon: {
+        enabled: true,
+      },
+      title: "New Idea",
+      text: ["Create a new idea."],
+      when: {
+        show: () => {
+          console.log("show step");
+        },
+        hide: () => {
+          console.log("hide step");
+        },
+      },
+    },
   ];
 
-
-  
   return (
     <ShepherdTour steps={steps} tourOptions={tourOptions}>
-    <Button  />
+      <Button />
 
-{children}
-
-</ShepherdTour>
-
-  )
+      {children}
+    </ShepherdTour>
+  );
 }
 
-export default ShephardLogic
-
-
+export default ShephardLogic;
 
 function Button() {
-    const tour = useContext(ShepherdTourContext);
-  
-    return (
-      <button className="hidden p-3 bg-t-bl rounded-xl step-1" onClick={tour.start}>
-        Start Tour
-      </button>
-    );
-  }
+  const tour = useContext(ShepherdTourContext);
+
+  return (
+    <button
+      className="hidden p-2 bg-t-bl rounded-xl step-1"
+      onClick={tour.start}
+    >
+      Start Tour
+    </button>
+  );
+}
