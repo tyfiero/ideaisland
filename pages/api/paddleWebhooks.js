fbf48793965751b3e4f9bee567fbaaba0dcee44c
tyfiero <77432183+tyfiero@users.noreply.github.com>
2022-05-10 13:13:39 -0700

    added additional console.log

--- pages/api/paddleWebhooks.js
+++ pages/api/paddleWebhooks.js
@@ -1,12 +1,16 @@
+
+
 const {verifyPaddleWebhook} = require('verify-paddle-webhook');
 
 export default async function handler(req, res) {
 if(!req.body) return res.status(401).json({error: 'No req body found'});
 
 
+console.log('HIT PADDLE WEBHOOK');
 
 if (verifyPaddleWebhook(process.env.PADDLE_PUBLIC_KEY, req.body)) {
     console.log('Webhook is valid!');
+
     // process the webhook
 
 if(req.body.alert_name === 'subscription_created') {
